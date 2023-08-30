18e0e1ac8e1858e2c50b3e74fb04b0b6feac9d77
SHAYAN PC <shayantanoli38@gmal.com>
2023-08-30 04:04:15 -0700

    Minor Changes...

--- lib/create_group.dart
+++ lib/create_group.dart
@@ -94,105 +94,140 @@ class _AddMembersState extends State<AddMembers> {
         duration: Duration(milliseconds: 200),
         child: selectedContacts.length >= 1
             ? Container(
-                child: Column(children: [
-                  Container(
-                    height: getheight(context) * 0.12,
-                    width: getwidth(context),
-                    child: ListView.builder(
-                        controller: _scrollController,
-                        scrollDirection: Axis.horizontal,
-                        itemCount: selectedContacts.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            height: getheight(context) * 0.05,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.only(left: 11, top: 8),
-                                  child: CircleAvatar(
-                                    // child: Text(
-                                    //     "${selectedContacts[index]["name"]}"),
-                                    backgroundColor: Colors.transparent,
-                                    backgroundImage:
-                                        AssetImage("assets/images/logo.png"),
-                                    radius: getheight(context) * 0.035,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: getheight(context) * 0.12,
+                        width: getwidth(context),
+                        child: ListView.builder(
+                            controller: _scrollController,
+                            scrollDirection: Axis.horizontal,
+                            itemCount: selectedContacts.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                height: getheight(context) * 0.05,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 11, top: 8),
+                                      child: CircleAvatar(
+                                        // child: Text(
+                                        //     "${selectedContacts[index]["name"]}"),
+                                        backgroundColor: Colors.transparent,
+                                        backgroundImage: AssetImage(
+                                            "assets/images/logo.png"),
+                                        radius: getheight(context) * 0.035,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 11),
+                                      child: Text(
+                                        "${selectedContacts[index]['name']}",
+                                        style: TextStyle(fontSize: 10),
+                                        softWrap: true,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              );
+                            }),
+                      ),
+                      Container(
+                        width: 300,
+                        margin:
+                            EdgeInsets.only(right: getwidth(context) * 0.05),
+                        padding: EdgeInsets.only(
+                          left: 8,
+                        ),
+                        height: getheight(context) * 0.1,
+                        child: TextField(
+                          showCursor: true,
+                          cursorColor: Color.fromRGBO(54, 94, 212, 1.0),
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.group,color: Color.fromRGBO(54, 94, 212, 1.0)),
+                            hintText: "Group Name",
+                            hintStyle: TextStyle(
+                                color: Color.fromRGBO(54, 94, 212, 0.75),
+                                fontWeight: FontWeight.w500),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                              color: Color.fromRGBO(54, 94, 212, 1.0),
+                              width: 2,
+                            )),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                              width: 1.5,
+                              color: Color.fromRGBO(54, 94, 212, 1.0),
+                            )),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: getheight(context) * 0.88,
+                          width: getwidth(context),
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            itemCount: allContacts.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              Map<String, dynamic> listofcontacts =
+                                  allContacts[index];
+                              return GestureDetector(
+                                onTap: () {
+                                  toggleSelection(
+                                      index); // Toggle selection on tap
+                                  if (listofcontacts['isSelected']) {
+                                    // Add the selected contact to the selectedContacts list
+                                    selectedContacts.add(listofcontacts);
+                                  } else {
+                                    // Remove the deselected contact from the selectedContacts list
+                                    selectedContacts.remove(listofcontacts);
+                                  }
+                                  print(selectedContacts);
+                                  _scrollController.animateTo(
+                                    _scrollController.position.maxScrollExtent,
+                                    // Scroll to the maximum extent
+                                    duration: Duration(milliseconds: 300),
+                                    // Set the duration of the animation
+                                    curve: Curves
+                                        .easeInOut, // Set the animation curve
+                                  );
+                                },
+                                child: ListTile(
+                                  leading:
+                                      Image.asset('assets/images/logo.png'),
+                                  title: Text(listofcontacts['name']),
+                                  subtitle: Text(listofcontacts['email']),
+                                  trailing: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: AnimatedSwitcher(
+                                      duration: Duration(milliseconds: 400),
+                                      child: listofcontacts['isSelected']
+                                          ? Icon(
+                                              Icons.done,
+                                              color: Color.fromRGBO(67, 97, 238,
+                                                  0.9490196078431372),
+                                            )
+                                          : Icon(
+                                              Icons.add,
+                                              color: Color.fromRGBO(67, 97, 238,
+                                                  0.9490196078431372),
+                                            ), // Don't use IconButton here
+                                    ),
                                   ),
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 11),
-                                  child: Text(
-                                    "${selectedContacts[index]['name']}",
-                                    style: TextStyle(fontSize: 10),
-                                    softWrap: true,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                )
-                              ],
-                            ),
-                          );
-                        }),
-                  ),
-                  Expanded(
-                    child: Container(
-                      height: getheight(context) * 0.88,
-                      width: getwidth(context),
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.vertical,
-                        itemCount: allContacts.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          Map<String, dynamic> listofcontacts =
-                              allContacts[index];
-                          return GestureDetector(
-                            onTap: () {
-                              toggleSelection(index); // Toggle selection on tap
-                              if (listofcontacts['isSelected']) {
-                                // Add the selected contact to the selectedContacts list
-                                selectedContacts.add(listofcontacts);
-                              } else {
-                                // Remove the deselected contact from the selectedContacts list
-                                selectedContacts.remove(listofcontacts);
-                              }
-                              print(selectedContacts);
-                              _scrollController.animateTo(
-                                _scrollController.position.maxScrollExtent,
-                                // Scroll to the maximum extent
-                                duration: Duration(milliseconds: 300),
-                                // Set the duration of the animation
-                                curve:
-                                    Curves.easeInOut, // Set the animation curve
                               );
                             },
-                            child: ListTile(
-                              leading: Image.asset('assets/images/logo.png'),
-                              title: Text(listofcontacts['name']),
-                              subtitle: Text(listofcontacts['email']),
-                              trailing: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: AnimatedSwitcher(
-                                  duration: Duration(milliseconds: 400),
-                                  child: listofcontacts['isSelected']
-                                      ? Icon(
-                                          Icons.done,
-                                          color: Color.fromRGBO(
-                                              67, 97, 238, 0.9490196078431372),
-                                        )
-                                      : Icon(
-                                          Icons.add,
-                                          color: Color.fromRGBO(
-                                              67, 97, 238, 0.9490196078431372),
-                                        ), // Don't use IconButton here
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  )
-                ]),
+                          ),
+                        ),
+                      )
+                    ]),
               )
             : Container(
                 height: getheight(context),
@@ -294,37 +329,37 @@ class _AddSubjectState extends State<AddSubject> {
           ),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              height: getheight(context) * 0.2,
-              width: getwidth(context),
-              child: Row(
-                children: [
-                  Text("Group Name:"),
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      labelText: "Group Name",
-                      labelStyle: TextStyle(
-                        color: Colors.blueAccent,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(
-                            color: Colors.blueAccent,
-                            width: 2,
-                          )),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide:
-                              BorderSide(color: Colors.blueAccent, width: 1)),
-                    ),
-                  ),
-                ],
+              width: 350,
+              margin: EdgeInsets.only(right: getwidth(context) * 0.05),
+              padding: EdgeInsets.only(left: 8, right: 15, top: 15, bottom: 15),
+              height: getheight(context) * 0.1,
+              child: TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  hintText: "Group Name",
+                  hintStyle: TextStyle(
+                      color: Color.fromRGBO(54, 94, 212, 0.7),
+                      fontWeight: FontWeight.w500),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(54, 94, 212, 1.0),
+                        width: 2,
+                      )),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                    width: 2,
+                    color: Color.fromRGBO(54, 94, 212, 1.0),
+                  )),
+                ),
               ),
             ),
             Container(
               height: getheight(context) * 0.8,
+              width: getwidth(context),
             )
           ],
         ));
